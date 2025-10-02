175251c0f5164da9e966816deb225c96ff940fad
Akinloye3264 <e.akinloye@alustudent.com>
2025-10-02 13:12:21 +0200

    created database setup

--- api_assignment.py
+++
@@ -1,3 +0,0 @@
-# - Download the api_security.py file from Canvas
-# - Improve the basic Password based Auth into Hashed API key based auth
-# - Enforce the Auth for your POST/PUT and DELETE handlers
